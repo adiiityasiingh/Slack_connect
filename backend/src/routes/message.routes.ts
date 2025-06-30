2c6dbdf6e0889f3d61dfa8d3a616f7e9d681675d
Aditya Kumar Singh <ad69832@gmail.com>
2025-07-01 05:18:37 +0530

    Update message.routes.ts

--- backend/src/routes/message.routes.ts
+++ backend/src/routes/message.routes.ts
@@ -9,9 +9,9 @@ import { cancelScheduledMessage } from '../controllers/message.controller';
 const router = express.Router();
 
 
-router.post('/message/send', sendMessageNow);
-router.post('/message/schedule', scheduleMessage);
-router.get('/message/scheduled/:team_id', getScheduledMessages);
-router.delete('/message/cancel/:id', cancelScheduledMessage);
+router.post('/send', sendMessageNow);
+router.post('/schedule', scheduleMessage);
+router.get('/scheduled/:team_id', getScheduledMessages);
+router.delete('/cancel/:id', cancelScheduledMessage);
 
 export default router;
